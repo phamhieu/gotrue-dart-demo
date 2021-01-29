bd4afccdd3008f2cb635856773402e4f421e565c
phamhieu <phamhieu1998@gmail.com>
2021-01-29 17:57:26 +0800

    refactor: gotrueClient params

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,11 +7,10 @@ const String PASSWORDRECOVER_SCREEN = 'PASSWORDRECOVER_SCREEN';
 
 const PERSIST_SESSION_KEY = 'PERSIST_SESSION_KEY';
 
-final gotrueClient = GoTrueClient(
-    url: 'https://nmbtadhxevkgnywohlie.supabase.net/auth/v1',
-    headers: {
-      'Authorization':
-          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxMTcxNjE0MiwiZXhwIjoxOTI3MjkyMTQyfQ.utotClF3wl5mRf4Hl9gC_HP3edyLX-3rs4RCnKrRuKQ',
-      'apikey':
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxMTcxNjE0MiwiZXhwIjoxOTI3MjkyMTQyfQ.utotClF3wl5mRf4Hl9gC_HP3edyLX-3rs4RCnKrRuKQ',
-    });
+const SUPABASE_URL = 'https://nmbtadhxevkgnywohlie.supabase.net';
+const SUPABASE_ANNON_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxMTcxNjE0MiwiZXhwIjoxOTI3MjkyMTQyfQ.utotClF3wl5mRf4Hl9gC_HP3edyLX-3rs4RCnKrRuKQ';
+final gotrueClient = GoTrueClient(url: '$SUPABASE_URL/auth/v1', headers: {
+  'Authorization': 'Bearer $SUPABASE_ANNON_KEY',
+  'apikey': SUPABASE_ANNON_KEY,
+});
